692b8e987cce4ccbba9cc8749fb5930a21a2c0a3
dema-xps <gg.dema.rm.22@gmail.com>
2023-06-10 10:24:30 +0200

    extend bayesLinear for include the possibility of sample the weight from the network. Fix main of the module and include some test for the speed

--- bnn.py
+++ bnn.py
@@ -2,12 +2,36 @@ from torchbnn.modules.linear import BayesLinear
 from torchbnn.modules.module import BayesModule
 from torchbnn.utils import freeze, unfreeze
 from torch.nn.functional import relu
+import torch.nn.functional as F
 
 
-'''
-this class should just implement the base bnn and backprop step for it
-consider the dynamics module for a complete dynamics model (that contains a set of bnn) 
-'''
+class BayesLayerWithSample(BayesLinear):
+    """
+        same of BayesLinear from torchbnn, just add the possibility of sample
+        a set of weight from the net. Functional paradigm for calc linear output
+    """
+    def sample_layer(self, device):
+        if self.weight_eps is None:
+            weight = (self.weight_mu + torch.exp(self.weight_log_sigma) * torch.randn_like(
+                self.weight_log_sigma)).detach()
+        else:
+            weight = (self.weight_mu + torch.exp(self.weight_log_sigma) * self.weight_eps).detach()
+        weight = weight.to(device)
+
+        if self.bias:
+            if self.bias_eps is None:
+                bias = (self.bias_mu + torch.exp(self.bias_log_sigma) * torch.randn_like(self.bias_log_sigma)).detach()
+            else:
+                bias = (self.bias_mu + torch.exp(self.bias_log_sigma) * self.bias_eps).detach()
+            bias = bias.to(device)
+        else:
+            bias = None
+
+        def linear_step(x):
+            return F.linear(x, weight, bias)
+
+        return linear_step
+
 
 class BNN(BayesModule):
     def __init__(self, action_dim, obs_dim, reward_dim, weight_world_model=None):
@@ -20,19 +44,19 @@ class BNN(BayesModule):
         self.h2_out_features = 128
         self.out_features = obs_dim + reward_dim
 
-        self.input_layer = BayesLinear(prior_mu=0, prior_sigma=1,
-                                       in_features=self.in_features,
-                                       out_features=self.h1_in_features)
-        self.hidden1_layer = BayesLinear(prior_mu=0, prior_sigma=1,
-                                         in_features=self.h1_in_features,
-                                         out_features=self.h1_out_features)
+        self.input_layer = BayesLayerWithSample(prior_mu=0, prior_sigma=1,
+                                          in_features=self.in_features,
+                                          out_features=self.h1_in_features)
+        self.hidden1_layer = BayesLayerWithSample(prior_mu=0, prior_sigma=1,
+                                            in_features=self.h1_in_features,
+                                            out_features=self.h1_out_features)
 
-        self.hidden2_layer = BayesLinear(prior_mu=0, prior_sigma=1,
-                                         in_features=self.h2_in_features,
-                                         out_features=self.h2_out_features)
-        self.ouput_layer = BayesLinear(prior_mu=0, prior_sigma=1,
-                                       in_features=self.h2_out_features,
-                                       out_features=self.out_features)
+        self.hidden2_layer = BayesLayerWithSample(prior_mu=0, prior_sigma=1,
+                                            in_features=self.h2_in_features,
+                                            out_features=self.h2_out_features)
+        self.ouput_layer = BayesLayerWithSample(prior_mu=0, prior_sigma=1,
+                                          in_features=self.h2_out_features,
+                                          out_features=self.out_features)
 
         if weight_world_model:
             self.copy_params_from_world_model(weight_world_model)
@@ -50,6 +74,18 @@ class BNN(BayesModule):
         except BaseException:
             print('non compatible W')
 
+    def sample_linear_net(self, device):
+        step = []
+        for layer in self._modules.items():
+            step.append(layer[1].sample_layer(device))
+
+        def forward_with_sample(x):
+            for op in step:
+                x = F.elu(op(x))
+            return x
+
+        return forward_with_sample
+
     def deterministic_mode(self):
         '''deterministic output'''
         freeze(self)
@@ -61,23 +97,79 @@ class BNN(BayesModule):
 
 if __name__ == '__main__':
     import torch
+    import time
+    dev = 'cuda:0'
+    test_freeze_unfreeze = False
+    test_functionalSample_vs_basicNet = True
 
-    basic_bnn = BNN(
+    basic_bnn_gpu = BNN(
+        action_dim=10,
+        obs_dim=10,
+        reward_dim=10
+    ).to(dev)
+
+    basic_bnn_cpu = BNN(
         action_dim=10,
         obs_dim=10,
         reward_dim=10
     )
 
-    x = torch.rand((20, ))
-    # here the 2 output should be stochastic
-    out1 = basic_bnn(x)
-    out2 = basic_bnn(x)
-    print(f'{out1=}')
-    print(f'{out2=}')
+    x_gpu = torch.rand((20,)).to(dev)
+    x_cpu = torch.rand((20, ))
 
-    basic_bnn.deterministic_mode()
+    if test_freeze_unfreeze:
+        # here the 2 output should be stochastic
+        basic_bnn_gpu.stochatisc_mode()
+        out1 = basic_bnn_gpu(x_gpu)
+        out2 = basic_bnn_gpu(x_gpu)
+        print(f'{out1=}, dev = {out1.get_device()}')
+        print(f'{out2=}, dev = {out2.get_device()}')
 
-    out1 = basic_bnn(x)
-    out2 = basic_bnn(x)
-    print(f'{out1=}')
-    print(f'{out2=}')
+        basic_bnn_gpu.deterministic_mode()
+
+        out1 = basic_bnn_gpu(x_gpu)
+        out2 = basic_bnn_gpu(x_gpu)
+        print(f'{out1=}')
+        print(f'{out2=}')
+
+        # restore old behaviour
+        basic_bnn_gpu.stochatisc_mode()
+
+    if test_functionalSample_vs_basicNet:
+        basic_bnn_gpu.deterministic_mode()
+        basic_bnn_cpu.deterministic_mode()
+
+        iter_numb = 1000
+        t_start = time.time()
+        for i in range(iter_numb):
+            _ = basic_bnn_cpu(x_cpu)
+        t_end = time.time()
+        has_grad = _.requires_grad
+        print(f'{iter_numb} step forward cpu NO SAMPLE:{t_end - t_start}, grad: {has_grad}')
+
+        t_start = time.time()
+        for i in range(iter_numb):
+            _ = basic_bnn_gpu(x_gpu)
+        t_end = time.time()
+        has_grad = _.requires_grad
+        print(f'{iter_numb} step forward gpu NO SAMPLE:{t_end - t_start}, grad: {has_grad}')
+
+
+
+        sample_cpu = basic_bnn_cpu.sample_linear_net(device='cpu')
+        t_start = time.time()
+        for i in range(iter_numb):
+            _ = sample_cpu(x_cpu)
+        t_end = time.time()
+        has_grad = _.requires_grad
+        print(f'{iter_numb} step forward cpu FUNCT:{t_end - t_start}, grad: {has_grad}')
+
+
+
+        sample_gpu = basic_bnn_gpu.sample_linear_net(device='cuda:0')
+        t_start = time.time()
+        for i in range(iter_numb):
+            _ = sample_gpu(x_gpu)
+        t_end = time.time()
+        has_grad = _.requires_grad
+        print(f'{iter_numb} step forward gpu FUNCT:{t_end - t_start}, grad: {has_grad}')
